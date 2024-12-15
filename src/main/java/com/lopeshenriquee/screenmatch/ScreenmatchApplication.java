4aca6b056532888ae16b5856e2ab0a38a2251f3d
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-14 23:31:04 -0300

    Printando todo os ep da temporada x no json

--- src/main/java/com/lopeshenriquee/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/lopeshenriquee/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,16 @@
 package com.lopeshenriquee.screenmatch;
 
 import com.lopeshenriquee.screenmatch.model.DataEpisodes;
+import com.lopeshenriquee.screenmatch.model.DataSeason;
 import com.lopeshenriquee.screenmatch.model.DataSeries;
 import com.lopeshenriquee.screenmatch.service.ConsumoAPI;
 import com.lopeshenriquee.screenmatch.service.DataConvert;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -30,6 +34,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 		DataEpisodes data2 = dataConvert.getData(jsonEpisode, DataEpisodes.class);
 		System.out.println(data2);
+
+		List<DataSeason> dataSeasonsList = new ArrayList<>();
+
+		for (int i = 0; i <= data1.totalSeasons() ; i++) {
+			var jsonSeason = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season="+ i +"&apikey=9287c99a");
+			DataSeason dataSeason = dataConvert.getData(jsonSeason, DataSeason.class);
+			dataSeasonsList.add(dataSeason);
+		}
+		dataSeasonsList.forEach(System.out::println);
 
 	}
 }
