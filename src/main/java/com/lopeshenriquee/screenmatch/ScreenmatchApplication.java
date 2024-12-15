fd6347562827863e631c2279a157e909a5351f9a
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-15 14:46:52 -0300

    segregando para uma principal, e trazendo a partir de scanner a serie, e mostrando temporadas

--- src/main/java/com/lopeshenriquee/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/lopeshenriquee/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.lopeshenriquee.screenmatch;
 import com.lopeshenriquee.screenmatch.model.DataEpisodes;
 import com.lopeshenriquee.screenmatch.model.DataSeason;
 import com.lopeshenriquee.screenmatch.model.DataSeries;
+import com.lopeshenriquee.screenmatch.principal.Principal;
 import com.lopeshenriquee.screenmatch.service.ConsumoAPI;
 import com.lopeshenriquee.screenmatch.service.DataConvert;
 import org.springframework.boot.CommandLineRunner;
@@ -21,28 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var jsonGeneral = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9287c99a");
-		var jsonEpisode = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=9287c99a");
-
-		System.out.println(jsonGeneral);
-		System.out.println(jsonEpisode);
-
-		DataConvert dataConvert = new DataConvert();
-		DataSeries data1 = dataConvert.getData(jsonGeneral, DataSeries.class);
-		System.out.println(data1);
-
-		DataEpisodes data2 = dataConvert.getData(jsonEpisode, DataEpisodes.class);
-		System.out.println(data2);
-
-		List<DataSeason> dataSeasonsList = new ArrayList<>();
-
-		for (int i = 0; i <= data1.totalSeasons() ; i++) {
-			var jsonSeason = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season="+ i +"&apikey=9287c99a");
-			DataSeason data3 = dataConvert.getData(jsonSeason, DataSeason.class);
-			dataSeasonsList.add(data3);
-		}
-		dataSeasonsList.forEach(System.out::println);
-
+		Principal principal = new Principal();
+		principal.showMenu();
 	}
 }
---
+++ src/main/java/com/lopeshenriquee/screenmatch/principal/Principal.java
@@ -0,0 +1,40 @@
+package com.lopeshenriquee.screenmatch.principal;
+
+import com.lopeshenriquee.screenmatch.model.DataSeason;
+import com.lopeshenriquee.screenmatch.model.DataSeries;
+import com.lopeshenriquee.screenmatch.service.ConsumoAPI;
+import com.lopeshenriquee.screenmatch.service.DataConvert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner scan = new Scanner(System.in);
+
+    private ConsumoAPI consumptionAPI = new ConsumoAPI();
+
+    private DataConvert dataConvert = new DataConvert();
+
+    private final String ADRESS = "https://www.omdbapi.com/?t=";
+
+    private final String APIKEY = "&apikey=9287c99a";
+
+    public void showMenu(){
+        System.out.println("Digite o nome da série");
+        var nameSerie = scan.nextLine();
+        var jsonGeneral = consumptionAPI.obterDados(ADRESS + nameSerie.replace(" ", "+") + APIKEY);
+
+        DataSeries data1 = dataConvert.getData(jsonGeneral, DataSeries.class);
+        System.out.println(data1);
+
+        List<DataSeason> seasonsList = new ArrayList<>();
+
+		for (int i = 0; i < data1.totalSeasons() ; i++) { //https://www.omdbapi.com/?t=gilmore+girls&Season="+ i +"&apikey=9287c99a"
+			var jsonSeason = consumptionAPI.obterDados(ADRESS + nameSerie.replace(" ", "+")+ "&Season="+ i + APIKEY);
+			DataSeason data2 = dataConvert.getData(jsonSeason, DataSeason.class);
+			seasonsList.add(data2);
+		}
+		seasonsList.forEach(System.out::println);
+    }
+}
