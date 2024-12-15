76885e9ec9125291e492e8a5a9952b3e4b8c6b55
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-14 23:19:43 -0300

    declarei url especificada e fiz o printei a request de episodios

--- src/main/java/com/lopeshenriquee/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/lopeshenriquee/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.lopeshenriquee.screenmatch;
 
+import com.lopeshenriquee.screenmatch.model.DataEpisodes;
 import com.lopeshenriquee.screenmatch.model.DataSeries;
 import com.lopeshenriquee.screenmatch.service.ConsumoAPI;
 import com.lopeshenriquee.screenmatch.service.DataConvert;
@@ -17,12 +18,18 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9287c99a");
+		var jsonGeneral = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9287c99a");
+		var jsonEpisode = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=9287c99a");
 
-		System.out.println(json);
+		System.out.println(jsonGeneral);
+		System.out.println(jsonEpisode);
 
 		DataConvert dataConvert = new DataConvert();
-		DataSeries data = dataConvert.getData(json, DataSeries.class);
-		System.out.println(data);
+		DataSeries data1 = dataConvert.getData(jsonGeneral, DataSeries.class);
+		System.out.println(data1);
+
+		DataEpisodes data2 = dataConvert.getData(jsonEpisode, DataEpisodes.class);
+		System.out.println(data2);
+
 	}
 }
