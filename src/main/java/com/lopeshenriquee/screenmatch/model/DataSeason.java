8f843acbbb001af9d5ac7be388a32cc093026fad
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-14 23:19:02 -0300

    criando classe dadosSeason

---
+++ src/main/java/com/lopeshenriquee/screenmatch/model/DataSeason.java
@@ -0,0 +1,10 @@
+package com.lopeshenriquee.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataSeason(@JsonAlias("Season") Integer number,
+                         @JsonAlias("Episodes") List<DataEpisodes> episodes) {
+}
