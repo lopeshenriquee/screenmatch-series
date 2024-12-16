b1e271727a3b8d840216422f11deee120095f203
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-16 19:28:44 -0300

    usando peek, para analisar como se fosse um breakpoint, acompanhando oq ele ta fazendo dentro do stream

--- src/main/java/com/lopeshenriquee/screenmatch/principal/Principal.java
+++ src/main/java/com/lopeshenriquee/screenmatch/principal/Principal.java
@@ -3,7 +3,6 @@ package com.lopeshenriquee.screenmatch.principal;
 import com.lopeshenriquee.screenmatch.model.DataEpisodes;
 import com.lopeshenriquee.screenmatch.model.DataSeason;
 import com.lopeshenriquee.screenmatch.model.DataSeries;
-import com.lopeshenriquee.screenmatch.model.Episodes;
 import com.lopeshenriquee.screenmatch.service.ConsumoAPI;
 import com.lopeshenriquee.screenmatch.service.DataConvert;
 
@@ -59,15 +58,37 @@ public class Principal {
         //TOP5
         dataEpisodesList.stream()
                 .filter(e -> !e.imdbRating().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A) " + e))
                 .sorted(Comparator.comparing(DataEpisodes::imdbRating).reversed())
-                .limit(5)
+                .peek(e -> System.out.println("Ordenação " + e))
+                .limit(10)
+                .peek(e -> System.out.println("Limite " + e))
+                .map(e -> e.title().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento " + e))
                 .forEach(System.out::println);
 
-        List<Episodes> episodes = seasonsList.stream()
-                .flatMap(season -> season.episodes().stream()
-                        .map(dataEpisode -> new Episodes(season.season(), dataEpisode))
-                ).collect(Collectors.toList());
+//        List<Episodes> episodes = seasonsList.stream()
+//                .flatMap(season -> season.episodes().stream()
+//                        .map(dataEpisode -> new Episodes(season.season(), dataEpisode))
+//                ).collect(Collectors.toList());
+//
+//        episodes.forEach(System.out::println);
+//
+//        System.out.println("A partir de que ano você deseja ver os episódios? ");
+//        var year = scan.nextInt();
+//        scan.nextLine();
+//
+//        LocalDate findDate = LocalDate.of(year, 1, 1);
+//
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        //filtro para trazer episódios a partir do ano x
+//        episodes.stream()
+//                .filter(e -> e.getRelease() != null && e.getRelease().isAfter(findDate))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getSeason() + " " +
+//                                "Episódio: " + e.getSeason() + " " +
+//                                "Data Lançamento: " + e.getRelease().format(formatter)
+//                ));
 
-        episodes.forEach(System.out::println);
     }
 }
